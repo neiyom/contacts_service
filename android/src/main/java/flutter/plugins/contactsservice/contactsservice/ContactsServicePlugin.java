4333c9f2ad4c9e2b4ea58bcf88b9ca2f7e6fa682
neiyom <info@neiyom.com>
2025-08-29 15:55:57 +0530

    Update ContactsServicePlugin.java

--- android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
+++ android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
@@ -1,71 +1,48 @@
 package flutter.plugins.contactsservice.contactsservice;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.ContactsContract;
+import android.content.Context;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
-public class ContactsServicePlugin implements FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
+public class ContactsServicePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
     private MethodChannel channel;
+    private Context context;
     private Activity activity;
-    private ContentResolver contentResolver;
 
     @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-        channel = new MethodChannel(binding.getBinaryMessenger(), "github.com/contacts_service");
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        context = flutterPluginBinding.getApplicationContext();
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "contacts_service");
         channel.setMethodCallHandler(this);
-        contentResolver = binding.getApplicationContext().getContentResolver();
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
+        channel = null;
     }
 
     @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-        switch (call.method) {
-            case "getContacts":
-                ContactsFetcher.fetchContacts(contentResolver, result, call);
-                break;
-            case "addContact":
-                ContactAdder.addContact(activity, call, result);
-                break;
-            case "deleteContact":
-                ContactDeleter.deleteContact(activity, call, result);
-                break;
-            case "updateContact":
-                ContactUpdater.updateContact(activity, call, result);
-                break;
-            case "openContactForm":
-                openContactForm(result);
-                break;
-            default:
-                result.notImplemented();
+    public void onMethodCall(MethodCall call, Result result) {
+        // TODO: Implement your contacts logic here
+        if (call.method.equals("getContacts")) {
+            // fetch contacts and return
+            result.success("Contacts fetched!");
+        } else {
+            result.notImplemented();
         }
     }
 
-    private void openContactForm(MethodChannel.Result result) {
-        if (activity == null) {
-            result.error("NO_ACTIVITY", "ContactsService requires a foreground activity.", null);
-            return;
-        }
-        Intent intent = new Intent(Intent.ACTION_INSERT, ContactsContract.Contacts.CONTENT_URI);
-        activity.startActivity(intent);
-        result.success(null);
-    }
-
+    // ---- ActivityAware (for permissions) ----
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         activity = binding.getActivity();
